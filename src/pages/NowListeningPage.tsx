77132c206cf278316a887756458f657f7b8b2ef2
Giovanni <gio.a.98@gmail.com>
2022-08-18 12:35:12 +0200

    now listening page update

--- src/pages/NowListeningPage.tsx
+++ src/pages/NowListeningPage.tsx
@@ -1,17 +1,27 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, isPlatform } from '@ionic/react';
-import { settings } from 'ionicons/icons';
+import { personCircleOutline, settings } from 'ionicons/icons';
+import { useEffect, useRef, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
+import LoginModal from '../components/LoginModal';
 import './NowListeningPage.css';
 
 const NowListeningPage: React.FC = () => {
+  const page = useRef(null)
+  const [presentingElement, setPresentingElement] = useState(null);
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
   return (
-    <IonPage>
+    <IonPage ref={page}>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Now listening</IonTitle>
           {!isPlatform('ios') && <IonButtons slot='secondary'>
-            <IonButton>
-              <IonIcon slot='icon-only' icon={settings}/>
+            <IonButton id="open-modal">
+              <IonIcon slot='icon-only' size='large' icon={personCircleOutline} />
+            </IonButton>
+            <IonButton >
+              <IonIcon slot='icon-only' icon={settings} />
             </IonButton>
           </IonButtons>}
         </IonToolbar>
@@ -21,14 +31,18 @@ const NowListeningPage: React.FC = () => {
           <IonToolbar>
             <IonTitle slot='secondary' size="large">Now listening</IonTitle>
             <IonButtons slot='primary'>
+              <IonButton id="open-modal">
+                <IonIcon slot='icon-only' size='large' icon={personCircleOutline} />
+              </IonButton>
               <IonButton >
-                <IonIcon slot='icon-only' icon={settings}/>
+                <IonIcon slot='icon-only' icon={settings} />
               </IonButton>
             </IonButtons>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Now listening page" />
       </IonContent>
+      <LoginModal page={page} presentingElement={presentingElement} />
     </IonPage>
   );
 };
